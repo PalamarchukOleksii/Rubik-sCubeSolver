55250c23b3aaf763b4d96c19b9f2649b3e2dc384
oleksiypalamarchuck@gmail.com <oleksiypalamarchuck@gmail.com>
2023-10-09 12:50:19 +0300

    виправив помилку

--- Rubik'sCubeSolver/Main.cpp
+++ Rubik'sCubeSolver/Main.cpp
@@ -6,6 +6,6 @@ int main() {
 	char startCube[] = { 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g',
 		'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y' };
 	RubiksCube cube(startCube);
-	cube.PrintCube();
+	cube.printCube();
 	return 0;
 }
