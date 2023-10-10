a3157e1f36ed9dc309b51c281d947433a9a09493
oleksiypalamarchuck@gmail.com <oleksiypalamarchuck@gmail.com>
2023-10-10 23:17:56 +0300

    .

--- Rubik'sCubeSolver/Main.cpp
+++ Rubik'sCubeSolver/Main.cpp
@@ -6,6 +6,7 @@ int main() {
 	char startCube[] = { 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g',
 		'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y' };
 	RubiksCube cube(startCube);
-	cube.PrintCube();
+	cube.makeAlgorithm({ "L'", "F2", "D2", "R", "B2", "U2", "F2", "U2", "L", "D2", "R2", "B", "D'", "L'", "R2", "D2", "B", "D2", "L", "R2", "U" });
+	cube.printCube();
 	return 0;
 }
